6c93ccdc60038261dc5c63951983ae1de9bb60ef
micheal briggs <michealsbusinessinbox@gmail.com>
2022-08-24 16:30:24 +0930

    placed foundations

---
+++ app.js
@@ -0,0 +1,25 @@
+// add, subtract, multiply, divide
+    function add(x,y){
+    return x + y;
+    } 
+console.log(add(4,5));
+
+    function subtract(x,y){
+    return x - y;
+    } 
+console.log(subtract(4,5));
+
+    function multiply(x,y){
+    return x * y;
+    } 
+console.log(multiply(4,5));
+
+    function divide(x,y){
+    return x / y;
+    } 
+console.log(divide(4,5));
+
+// operate function
+    function operate(){
+
+    }
